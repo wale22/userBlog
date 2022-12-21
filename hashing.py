9933b8f49d32fcb59224b6c4a0ab1bf598141b98
wale22 <walemustapha08@gmail.com>
2022-12-21 07:20:13 +0100

    update on hashing functionalities

--- hashing.py
+++ hashing.py
@@ -6,3 +6,6 @@ crypt_pwd=CryptContext(schemes=["bcrypt"])
 class Hash:
     def encrypt(password):
         return crypt_pwd.hash(password)
+    
+    def decrypt(plain_password,hashedpassword):
+        crypt_pwd.verify(plain_password, hashedpassword)
